b387cc3b358d9f204cfd7b0bffdeb31572a12380
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-05-20 16:38:14 +0900

    Lesson 7 対応

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ render((
     <Route path="/" component={App}>
       <Route path="/repos" component={Repos}/>
       {/* add the new route */}
-      <Route path="/repos/:userName/:repoName" component={Repo}/>
+      <Route path="/repos" component={Repos}>
+        <Route path="/repos/:userName/:repoName" component={Repo}/>
+      </Route>
       <Route path="/about" component={About}/>
     </Route>
   </Router>
--- modules/Repos.js
+++ modules/Repos.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router'
+import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
     return (
       <div>
         <h2>Repos</h2>
-
-        {/* add some links */}
         <ul>
-          <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
-          <li><Link to="/repos/facebook/react">React</Link></li>
+          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
         </ul>
-
+        {/* will render `Repo.js` when at /repos/:userName/:repoName */}
+        {this.props.children}
       </div>
     )
   }
