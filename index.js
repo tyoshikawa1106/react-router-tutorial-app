adc623cca3dea0dabc4b15f53253d8b9e20dddc3
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-05-20 15:55:11 +0900

    Lesson 6 対応

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import App from './modules/App'
 import { Router, Route, hashHistory } from 'react-router'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
-      {/* make them children of `App` */}
       <Route path="/repos" component={Repos}/>
+      {/* add the new route */}
+      <Route path="/repos/:userName/:repoName" component={Repo}/>
       <Route path="/about" component={About}/>
     </Route>
   </Router>
