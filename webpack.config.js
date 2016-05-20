4ea6201b4e585450149682307da74dbb74cab4c7
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-05-20 21:03:21 +0900

    Lesson 11 対応

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
+var webpack = require('webpack')
+
 module.exports = {
   entry: './index.js',
 
   output: {
+    path: 'public',
     filename: 'bundle.js',
     publicPath: ''
   },
+
+  // add this handful of plugins that optimize the build
+  // when we're in production
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : [],
+
 
   module: {
     loaders: [
