8afb61ad8a01e23a9403c015093c5129a0c9be2b
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-05-20 21:12:20 +0900

    Lesson 12 対応

--- modules/Repos.js
+++ modules/Repos.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router'
 import NavLink from './NavLink'
+import { browserHistory } from 'react-router'
 
 export default React.createClass({
+
+  // add this method
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    const path = `/repos/${userName}/${repo}`
+    browserHistory.push(path)
+    console.log(path)
+  },
+
   render() {
     return (
       <div>
@@ -10,8 +21,15 @@ export default React.createClass({
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+          {/* add this form */}
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
-        {/* will render `Repo.js` when at /repos/:userName/:repoName */}
         {this.props.children}
       </div>
     )
